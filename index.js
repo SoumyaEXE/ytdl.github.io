d281d74013f74a7b68f2f119bca49e5a00209fb5
Soumyadeep Dey <95275364+SoumyaEXE@users.noreply.github.com>
2023-04-08 20:15:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,196 +1,92 @@
-const express = require('express');
-const {chain, last, forEach} = require('lodash');
-const ytdl = require('ytdl-core');
-const {validate, Joi} = require('express-validation');
-const {spawn} = require('child_process');
-const sanitize = require('sanitize-filename');
-const ffmpegPath = require('ffmpeg-static');
-
-const app = express();
-
-app.use(express.static('public'))
-
-const getResolutions = (formats) =>
-	chain(formats)
-		.filter('height')
-		.map('height')
-		.uniq()
-		.orderBy(null, 'desc')
-		.value();
+const startBtn = document.querySelector('.start-btn')
+const videoURLInput = document.querySelector('.video-url-input')
+const videoThumbnailImg = document.querySelector('.video-thumbnail')
+const videoTitleDiv = document.querySelector('.video-title')
+const resolutionsDiv = document.querySelector('.resolutions')
+const downloadBtn = document.querySelector('.download-btn')
+const videoResolutionsSection = document.querySelector('.video-resolutions-section')
+const videoInfoSection = document.querySelector('.video-info-section')
+const videoForamtsSection = document.querySelector('.video-formats-section')
 
 
-app.get(
-	'/api/video',
-	validate({
-		query: Joi.object({
-			id: Joi.string().required(),
-		}),
-	}),
-	(req, res, next) => {
-		const {id} = req.query;
+const getVideoID = () => {
+    const url = videoURLInput.value
+    const searchParams = new URLSearchParams(url.split('?')[1])
+    return url
+}
 
-		ytdl.getInfo(id)
-			.then(({videoDetails, formats}) => {
-				const {title, thumbnails} = videoDetails;
+const getVideoInfo = async (id) => {
+    const res = await fetch(`/api/video?id=${id}`)
+    return res.json()
+}
 
-				const thumbnailURL = last(thumbnails).url;
+const show = el => el.classList.remove('d-none')
 
-				const resolutions = getResolutions(formats);
+const showResolutions = resolutions => {
+    const html = resolutions
+        .map((resolution, i) => `
+            <option name="resolution" value="${resolution}">${resolution}</option>
+        `)
+        .join('')
+    
+    resolutionsDiv.innerHTML = html
+}
 
-				res.json({title, thumbnailURL, resolutions});
-			})
-			.catch((err) => next(err));
-	},
-);
+const getRadioValue = name =>
+    document.querySelector(`[name="${name}"]:checked`).value
 
-app.get(
-	'/download',
-	validate({
-		query: Joi.object({
-			id: Joi.string(),
-			format: Joi.valid('video', 'audio'),
-			resolution: Joi.when(
-				'format',
-				{
-					is: Joi.valid('video'),
-					then: Joi.number().required(),
-				},
-			),
-		}),
-	}),
-	(req, res, next) => {
-		const {id, format} = req.query;
+const getDownloadAnchor = ({id, resolution, format}) => {
+    let url = `/download?id=${id}&format=${format}`
+    
+    if (format === 'video') {
+        url += `&resolution=${resolution}`
+    }
 
-		ytdl.getInfo(id)
-			.then(({videoDetails, formats}) => {
-				const {title} = videoDetails;
+    const a = document.createElement('a')
+    a.href = url
+    a.download = true
 
-				const streams = {};
+    return a
+}
 
-				if (format === 'video') {
-					const resolution = parseInt(req.query.resolution);
+const download = ({id, resolution, format}) => {
+    const a = getDownloadAnchor({id, resolution, format})
+    a.click()
+}
 
-					const resolutions = getResolutions(formats);
+startBtn.addEventListener(
+  
+    'click',
+    async () => {
+      const videoURL = videoURLInput.value.trim()
+            if (videoURL.length == 0) {
+                Swal.fire({
+  title: 'Error!',
+  text: 'Harap untuk memasukan URL!',
+  icon: 'error',
+  confirmButtonText: 'Oke'
+})
+            } 
+      
+        const id = getVideoID()
+        const {title, resolutions, thumbnailURL} = await getVideoInfo(id)
+        
+        show(videoForamtsSection)
+        
+        
+        videoTitleDiv.textContent = title
+        videoThumbnailImg.src = thumbnailURL
+        showResolutions(resolutions)
+    }
+)
 
-					if (!resolutions.includes(resolution)) {
-						return next(new Error('Resolution is incorrect'));
-					}
-
-					const videoFormat = chain(formats)
-						.filter(({height, videoCodec}) => (
-							height === resolution && videoCodec?.startsWith('avc1')
-						))
-						.orderBy('fps', 'desc')
-						.head()
-						.value();
-	
-					streams.video = ytdl(id, {quality: videoFormat.itag});
-					streams.audio = ytdl(id, {quality: 'highestaudio'});
-				}
-
-				if (format === 'audio') {
-					streams.audio = ytdl(id, {quality: 'highestaudio'});
-				}
-
-				const exts = {
-					video: 'mp4',
-					audio: 'mp3',
-				};
-
-				const contentTypes = {
-					video: 'video/mp4',
-					audio: 'audio/mpeg',
-				};
-
-				const ext = exts[format];
-				const contentType = contentTypes[format];
-				const filename = `${encodeURI(sanitize(title))}.${ext}`;
-
-				res.setHeader('Content-Type', contentType);
-				res.setHeader('Content-Disposition', `attachment; filename=${filename}; filename*=utf-8''${filename}`);
-
-				const pipes = {
-					out: 1,
-					err: 2,
-					video: 3,
-					audio: 4,
-				};
-
-				const ffmpegInputOptions = {
-					video: [
-						'-i', `pipe:${pipes.video}`,
-						'-i', `pipe:${pipes.audio}`,
-						'-map', '0:v',
-						'-map', '1:a',
-						'-c:v', 'copy',
-						'-c:a', 'libmp3lame',
-						'-crf', '27',
-						'-preset', 'veryfast',
-						'-movflags', 'frag_keyframe+empty_moov',
-						'-f', 'mp4',
-					],
-					audio: [
-						'-i', `pipe:${pipes.audio}`,
-						'-c:a', 'libmp3lame',
-						'-vn',
-						'-ar', '44100',
-						'-ac', '2',
-						'-b:a', '192k',
-						'-f', 'mp3',
-					],
-				};
-
-				const ffmpegOptions = [
-					...ffmpegInputOptions[format],
-					'-loglevel', 'error',
-					'-',
-				];
-
-				const ffmpegProcess = spawn(
-					ffmpegPath,
-					ffmpegOptions,
-					{
-						stdio: ['pipe', 'pipe', 'pipe', 'pipe', 'pipe'],
-					},
-				);
-
-				const handleFFmpegStreamError = (err) =>
-					console.error(err);
-
-				forEach(streams, (stream, format) => {
-					const dest = ffmpegProcess.stdio[pipes[format]];
-					stream.pipe(dest).on('error', handleFFmpegStreamError);
-				});
-
-				ffmpegProcess.stdio[pipes.out].pipe(res);
-
-				let ffmpegLogs = '';
-				ffmpegProcess.stdio[pipes.err].on(
-					'data',
-					(chunk) => ffmpegLogs += chunk.toString(),
-				);
-
-				ffmpegProcess.on(
-					'exit',
-					(exitCode) => {
-						if (exitCode === 1) {
-							console.error(ffmpegLogs);
-						}
-						res.end();
-					},
-				);
-
-				res.on(
-					'close',
-					() => ffmpegProcess.kill(),
-				);
-			})
-			.catch((err) => next(err));
-	},
-);
-
-const port = process.env.PORT;
-app.listen(
-	port,
-	() => console.log(`Server listening on port ${port}`),
-);
+downloadBtn.addEventListener(
+    'click',
+    () => {
+        download({
+            id: getVideoID(),
+            resolution: getRadioValue('resolution'),
+            format: getRadioValue('format')
+        })
+    }
+)
